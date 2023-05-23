76a30e76b9298a8ca5c12dc85a0c27fda2acdbe4
ShiGame45 <shiven.sheth@gmail.com>
2023-05-23 15:48:01 +0200

    Moved home to screen folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-
-import HomeScreen from './components/Home.js';
-import DashboardScreen from './components/Dashboard.js';
+import HomeScreen from './screens/Home.js';
 
 
 
